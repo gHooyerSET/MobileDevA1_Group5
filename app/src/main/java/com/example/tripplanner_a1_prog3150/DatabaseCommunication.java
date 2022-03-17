b914e8ce93fc3a69315608e1acd482eabba9e5b3
Patrick Cho <pcho3098@conestogac.on.ca>
2022-03-17 19:53:18 -0400

    Added DB communication class, removed mysql script

---
+++ app/src/main/java/com/example/tripplanner_a1_prog3150/DatabaseCommunication.java
@@ -0,0 +1,10 @@
+package com.example.tripplanner_a1_prog3150;
+
+public class DatabaseCommunication {
+    dbHelper = new DBHelper(con
+    Trip getTripByIndex(int tripNum) {
+        Trip oldTrip = new Trip();
+
+
+    }
+}
