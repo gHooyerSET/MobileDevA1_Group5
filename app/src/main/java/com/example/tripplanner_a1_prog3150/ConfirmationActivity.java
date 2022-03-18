00b48ce74f17dbf18f5880c6de9d55b6e3127938
gHooyerSET <ghooyer6402@conestogac.on.ca>
2022-03-18 10:13:42 -0400

    Added asynchronous operations, chained together in ConfirmationActivity.

--- app/src/main/java/com/example/tripplanner_a1_prog3150/ConfirmationActivity.java
+++ app/src/main/java/com/example/tripplanner_a1_prog3150/ConfirmationActivity.java
@@ -7,12 +7,20 @@
  */
 package com.example.tripplanner_a1_prog3150;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class ConfirmationActivity extends MenuActivity {
 
@@ -22,6 +30,7 @@ public class ConfirmationActivity extends MenuActivity {
     private TextView confirmationTotalCostTextView;
     private Button newTripButton;
     private Trip trip;
+    private TripDB tripDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,15 @@ public class ConfirmationActivity extends MenuActivity {
         confirmationTotalCostTextView.setText("$" + String.format("%.02f",trip.getTotalCost()));
         confirmationOriginTextView.setText(trip.getOrigin());
         confirmationDestinationTextView.setText(trip.getDestination());
+
+
+        // Use a toast to notify the user about the asynchronous operation beginning
+        //Create the tripDB object
+        tripDB = new TripDB(this);
+        // Start the asynchronous task to create the database entry
+        // and create a 'receipt' file as an asynchronous operation
+        new ASyncDatabaseCreator().execute(trip,tripDB);
+
         //Set up event handler
         //This button sends us back to the MainActivity screen
         newTripButton.setOnClickListener(view -> {
@@ -57,5 +75,70 @@ public class ConfirmationActivity extends MenuActivity {
              */
             ConfirmationActivity.this.startActivity(nextScreenIntent);
         });
+    }
+
+    public class ASyncFileWriter extends AsyncTask
+    {
+
+        @Override
+        protected Object doInBackground(@NonNull Object[] params) {
+            String fileString = "";
+            byte[] fileBuffer;
+            String filename = "";
+            // Create the file name
+            filename = trip.getTripId() + "_" + trip.getDestination() + "_" + "trip.txt";
+            // Create file data in the string
+            fileString = "Trip ID: " + trip.getTripId() + "\n";
+            fileString += "Origin: " + trip.getOrigin() + "\n";
+            fileString += "Destination: " + trip.getDestination() + "\n";
+            fileString += "Trip Goers: " + trip.getTripGoers() + "\n";
+            fileString += "# of Nights: " + String.format("%.2f",trip.getNights()) + "\n";
+            fileString += "Amenities Cost: $" + String.format("%.2f",trip.getAmenitiesCost()) + "\n";
+            fileString += "Hotel Cost: $" + String.format("%.2f",trip.getHotelCost()) + " / night\n";
+            fileString += "Total Hotel Cost: $" + String.format("%.2f",trip.getTotalHotelCost()) + "\n";
+            fileString += "Ticket Price: $" + String.format("%.2f",trip.getTicketPrice()) + "\n";
+            fileString += "Total Ticket Cost: $" + String.format("%.2f",trip.getTotalTicketCost()) + "\n";
+            fileString += "Total Cost: $" + String.format("%.2f",trip.getTotalCost()) + "\n";
+            try
+            {
+                // Create the output filestream
+                FileOutputStream out = openFileOutput(filename, Context.MODE_PRIVATE);
+                // Encode the string to bytes
+                fileBuffer = fileString.getBytes(StandardCharsets.UTF_8);
+                // Write the string
+                out.write(fileBuffer,0,fileBuffer.length);
+                // Close the file
+                out.close();
+            } catch (Exception e) {
+                // Log the exception
+                Log.d(String.format("%d",e.hashCode()),e.toString());
+            }
+            return null;
+        }
+    }
+
+    public class ASyncDatabaseCreator extends AsyncTask
+    {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            try {
+                // Insert the trip
+                tripDB.insertTrip(trip);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Object[] values) {
+            super.onProgressUpdate(values);
+
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            new ASyncFileWriter().execute(trip);
+        }
     }
 }
