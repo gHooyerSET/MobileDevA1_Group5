be7f5d2b1bc1c802faae1e84fe79e29b1c7d7bd1
Patrick Cho <pcho3098@conestogac.on.ca>
2022-04-11 22:17:08 -0400

    Add send trip activity page

---
+++ app/src/main/java/com/example/tripplanner_a1_prog3150/SendTripActivity.java
@@ -0,0 +1,13 @@
+/*
+ * FILE          : SendTripActivity.java
+ * PROJECT       : PROG3150 - Assignment #1 - Trip Planner
+ * FIRST VERSION : 2022-04-11
+ * PROGRAMMER    : Patrick Cho
+ * DESCRIPTION   : The code behind for the confirmation page.
+ */
+
+package com.example.tripplanner_a1_prog3150;
+
+public class SendTripActivity {
+    
+}
