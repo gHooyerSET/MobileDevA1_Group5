3a68bd2f1ce178ba5cbd6309afbba1c294efa9d8
Patrick Cho <pcho3098@conestogac.on.ca>
2022-04-12 20:59:26 -0400

    Completed for page

--- app/src/main/java/com/example/tripplanner_a1_prog3150/SendTripActivity.java
+++ app/src/main/java/com/example/tripplanner_a1_prog3150/SendTripActivity.java
@@ -8,9 +8,25 @@
 
 package com.example.tripplanner_a1_prog3150;
 
-public class SendTripActivity {
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class SendTripActivity extends MenuActivity {
     //On this page, we will be able to select from dropdown list of all contacts
         //There will be two buttons, send - or go back
             //Send will not do anything for now - out of assignment scope
             //Go back will return to previous page
+
+    //Create variables
+    private Spinner contactSpinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_trip);
+
+        //Initialize Variable
+
+    }
 }
